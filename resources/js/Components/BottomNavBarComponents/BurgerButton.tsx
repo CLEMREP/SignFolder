340e5fbe30fa3dd896aa18081f0f1c60d4b9f069
MaxVzl <maxime.vozelle@gmail.com>
2023-03-31 18:52:46 +0200

    :sparkles: manage side bar mobile with new bottom nav bar

--- resources/js/Components/BottomNavBarComponents/BurgerButton.tsx
+++ resources/js/Components/BottomNavBarComponents/BurgerButton.tsx
@@ -5,7 +5,7 @@ import {faDashboard, faSliders} from "@fortawesome/free-solid-svg-icons";
 export default function BurgerButton({routeName}: { routeName: string}) {
     return (
         <div className="fixed block md:hidden w-full bottom-0 p-5">
-            <div className="flex flex-row gap-2 justify-around items-center w-full p-5 rounded-full bg-primary-main">
+            <div className="flex flex-row gap-2 justify-around items-center w-full p-5 rounded-3xl bg-primary-main">
                 <Button icon={faDashboard} text={"Tableau de bord"} selected={routeName == 'dashboard' ?? true} routeName={'dashboard'}  />
                 <Button icon={faFolder} text={"Tableau de bord"} selected={routeName == 'folder' ?? true} routeName={'folder'}  />
                 <Button icon={faBuilding} text={"Tableau de bord"} selected={routeName == 'organization' ?? true} routeName={'organization'}  />
